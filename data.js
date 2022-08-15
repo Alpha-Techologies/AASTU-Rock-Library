97a69ec55caedab58bc6de90692632fa9c9c7451
Yohannes Teshome <83913756+John-droid-dotcom@users.noreply.github.com>
2022-08-15 15:20:29 +0300

    First Commit

---
+++ data.js
@@ -0,0 +1,163 @@
+
+export const data = {
+    "title":"Alexandrite",
+    "name": {
+      "blog":"blog blog blog",
+      "origin":"origin",
+      "aliases": "alias"
+    },
+    "image": {
+        "thumbnail": {
+            "path": "../image/path",
+            "caption": "image Caption"
+            },
+        "polished":{
+            "path": "../image/path",
+            "caption": "image Caption"
+            },
+        "else":[
+           {
+            "path": "../image/path",
+            "caption": "image Caption"
+           },
+           {
+          "path": "../image/path",
+          "caption": "image Caption"
+          }
+        ]},
+    "mineral":{
+    "blog":"blog blog blog",
+    "mineral":[
+    {
+        "name":"crystal name",
+        "class":"crystal class",
+        "mineral":"crystal mineral"
+    },
+    {
+        "name":"crystal name2",
+        "class":"crystal class2",
+        "mineral":"crystal mineral2"
+    }]
+    },
+    "genetics":{
+        "group":"igneous",
+        "origin":"sedimentary",
+        "faecies":"rock faecies"
+    },
+    "reference":[
+    {
+    "description": "A short description of the referenced article or journal",
+    "author":  "author",
+    "title": "title" ,
+    "publicationYear": "2004" ,
+    "link": "http://www.link to ref.com"
+    },
+    {
+    "description": "short description of the referenced article or journal2",
+    "author":  "author2",
+    "title": "title2" ,
+    "publicationYear": "2004" ,
+    "link": "http://www.link to ref.com2"
+    }],
+    "location": { 
+        "blog":"blog blog blog",
+        "place":  [
+    {
+      "name": "location name",
+      "coordinates": "1.1111",
+      "city": "city2",
+      "region": "region2"
+    },
+    {
+      "name": "location name",
+      "coordinates": "1.1111",
+      "city": "city",
+      "region": "region"
+    },
+    {
+      "name": "location name2",
+      "coordinates": "2.22222",
+      "city": "city",
+      "region": "region"
+    }]
+    },
+    "industry":{
+     "blog":"blog blog blog",
+    "industry":[
+      {
+        "name":"chemical",
+        "use":["bleaching agent","bleaching agent"]
+      },
+      {
+        "name":"chemical 2",
+        "use":["bleaching agent2","bleaching agent2"]
+      }
+    ]
+    },
+    "description":"<h2>Peridot History and Lore</h2><p><s>Peridot</s> has <sup>a</sup>lway<sub>s</sub> been <code>associated</code> <em>with</em> <span style=font-family: symbol; data-mce-style=font-family: symbol;>light</span>. <span style=font-size: 14pt; data-mce-style=font-size: 14pt;>In fact, the Egyptians</span> called it the “<span style=color: rgb(230, 126, 35); data-mce-style=color: rgb(230, 126, 35);>gem of the sun.”</span> Some believed that it protected its owner from “terrors of the night,” especially when it was set in gold. Others strung the gems on donkey hair and tied them around their left arms to ward off evil spirits.</p><p>The word peridot comes from the Arabic “faridat,” which means “gem.” Most peridot formed deep inside the earth and was delivered to the surface by volcanoes. Some also came to earth in meteorites, but this extraterrestrial peridot is extremely rare, and not likely to be seen in a retail jewelry store.</p><p>Early records indicate that the ancient Egyptians mined a beautiful green gem on an island in the Red Sea called Topazios, now known as St. John’s Island or Zabargad. Legend has it that the island was infested with snakes, making mining unpleasant until an enterprising pharaoh drove them into the sea. From the earliest times, people confused this stone—now known to be peridot—with other gems. It was one of many labeled as “topaz.”</p><p>Some historians believe that Cleopatra’s famous emerald collection might actually have been peridot. People in medieval times continued to confuse peridot with emerald. For centuries, people believed the fabulous 200-ct. gems adorning the shrine of the Three Holy Kings in Germany’s Cologne Cathedral were emeralds. They are, in fact, peridots.&nbsp;</p>."
+  ,
+  "chemicalProp": {
+        "blog": "blog blog blog",
+        "composition": { "element": ["element","element"] },
+        "percent":  [78,22] ,
+        "formula": "chemical formula",
+      "tabularInfo":[
+      {
+          "key":"key",
+          "value":"value2",
+          "factStatus":true,
+          "group":"groupName"
+      },
+      {
+          "key":"key",
+          "value":"value2",
+          "factStatus":true,
+          "group":"groupName"
+      }
+      ]
+      } ,
+      "physicalProp": {
+        "blog":" { type: String }",
+        "color": "{ type: String }",
+        "luminescence":" { type: String }",
+        "hardness": 21,
+        "conductivity":" { type: String }",
+        "density":21,
+        "meltingPt":78,
+        "cleavage_fracture":" { type: String }",
+        "tabularInfo": [
+      {
+          "key": "{type: String}",
+          "value":" {type: String}",
+          "factStatus": true,
+          "group":" {type: String}"
+      },
+      {
+          "key": "{type: String}",
+          "value":" {type: String}",
+          "factStatus": true,
+          "group":" {type: String}"
+      }
+    ]
+        },
+      "mechanicalProp": {
+        "blog": "{ type: String }",
+        "shearStrength": "{ type: String }",
+        "ductility":" { type: String }",
+        "tensileStrength": "{ type: String }",
+        "tabularInfo":[ 
+        {
+          "key": "{type: String}",
+          "value": "{type: String}",
+          "factStatus": true,
+          "group": "{type: String}"
+        },
+        {
+          "key": "{type: String}",
+          "value": "{type: String}",
+          "factStatus": true,
+          "group": "{type: String}"
+        }
+    ]
+      }
+  }
